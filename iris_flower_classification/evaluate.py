937968d4750212ee55e3e55b8f25d30041b8e497
Eric Dasse <christ.gououi@gmail.com>
2024-08-14 19:50:04 +0200

    Add `load_data` function

--- iris_flower_classification/evaluate.py
+++ iris_flower_classification/evaluate.py
@@ -14,6 +14,14 @@ def load_model(model_path: Path):
     return joblib.load(model_path)
 
 
+def load_data(dataset_path: Path) -> tuple[pd.Series, pd.Series]:
+    iris_data = pd.read_csv(dataset_path)
+    X = iris_data.drop("class", axis=1).values
+    y = iris_data["class"].values
+
+    return X, y
+
+
 def evaluate(y_test: pd.Series, y_pred: pd.Series) -> dict:
     """Evaluate classification and returns metrics."""
 
