dc1bd2831fec7167e739c9cdc567f17672b72085
Eric Dasse <christ.gououi@gmail.com>
2024-08-14 21:58:47 +0200

    Factorize data preparation

--- iris_flower_classification/prepare.py
+++ iris_flower_classification/prepare.py
@@ -7,6 +7,15 @@ from sklearn.model_selection import train_test_split
 from iris_flower_classification.data import dump_data, load_data
 
 
+def prepare(X, y):
+    y = pd.Categorical(y).codes
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=21
+    )
+
+    return X_train, X_test, y_train, y_test
+
+
 def _get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--raw-dataset-path", "-r")
@@ -26,10 +35,7 @@ def main():
     _check_is_dir(prepared_data_dir)
 
     X, y = load_data(raw_dataset_path)
-    y = pd.Categorical(y).codes
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=21
-    )
+    X_train, X_test, y_train, y_test = prepare(X, y)
 
     dump_data(X_train, y_train, path=f"{prepared_data_dir}/train.csv")
     dump_data(X_test, y_test, path=f"{prepared_data_dir}/test.csv")
